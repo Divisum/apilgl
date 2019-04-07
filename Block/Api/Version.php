a25a9090c2667e0023f2aa0a6b9236594c631597
Divisum <bgaiteiro@gmail.com>
2019-04-07 21:19:41 +0100

    initial commit

---
+++ Block/Api/Version.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Divisum\Apilgl\Block\Api;
+
+class Version extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
